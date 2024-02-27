fa1f47217c90e3c429bddc59b72af086abeaa11d
abrantedevops <abrante.devops@gmail.com>
2024-02-27 11:43:59 -0300

    database migrations

--- prisma/migrations/20240223223825_schame_nome/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `nome` to the `Cliente` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Cliente` ADD COLUMN `nome` VARCHAR(191) NOT NULL;
